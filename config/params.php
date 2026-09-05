ea8bd9623daa45ebf67c7e57d382c5a6e61b175d
user49324809 <160784140+user49324809@users.noreply.github.com>
2026-09-05 17:01:55 +0300

    Improve config/params.php

--- config/params.php
+++ config/params.php
@@ -4,4 +4,7 @@ return [
     'bsVersion' => '5.x',
     'shortCodeLength' => 7,
     'requestTimeout' => 8,
+    'trustProxyHeaders' => filter_var(getenv('TRUST_PROXY_HEADERS') ?: 'false', FILTER_VALIDATE_BOOL),
+    'adminUsername' => getenv('ADMIN_USERNAME') ?: '',
+    'adminPassword' => getenv('ADMIN_PASSWORD') ?: '',
 ];
